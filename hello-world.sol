e186353b9d014c9ee34d82d22766c68a03767b00
Yunus Ünal <72989533+unalyunus@users.noreply.github.com>
2022-05-03 01:40:46 +0300

    Update hello-world.sol

--- hello-world.sol
+++ hello-world.sol
@@ -1,26 +1,28 @@
- //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7; //define solidity version
 
 
-contract HelloWorld {
+contract HelloWorld { //start smart contract
 
-    mapping(uint => bool) num;
-    bool public b;
+    
+    mapping(uint => bool) num; // mapping for storing the data 
+    bool public b; // define a boolean state variable
 
-    function storeNumber(uint _num) public returns (bool) {
-        if(!num[_num]){
-            num[_num] = true;
-            b = num[_num];
+    function storeNumber(uint _num) public returns (bool) { //function that take uint and store it then return a boolean
+        if(!num[_num]){ // if it is true 
+            num[_num] = true; // true value assign to the uint that entering number is mapped
+            b = num[_num]; // a assign for checking the value changing
         }
-        return num[_num];
+        return num[_num]; // for testing the value
     }
 
-    function retrieveNumber(uint _num) public returns(bool){
-        if(num[_num]){
-            delete num[_num];
-            b = num[_num];
+    function retrieveNumber(uint _num) public returns(bool){ //function that take uint and store it then return a boolean
+        if(num[_num]){ // if it is true
+            delete num[_num];  // delete the value of that is mapped
+            b = num[_num];// a assign for checking the value changing
         }
-        return num[_num];
+        return num[_num];// for testing the value
     }
 
 }
